27b67086db3adabe1387ebeead5ddcd3fd704a37
Maxson C. Maidlinger <max_maidlinger@hotmail.com>
2019-04-09 07:18:18 -0300

    watson inplementation

--- credentials/README.md
+++ credentials/README.md
@@ -6,3 +6,18 @@
     "apiKey": "API_KEY_ALGORITHMIA"
 }
 ```
+
+* **Watson Natural Language Understanding**
+* Create the file **watson-nlu.json**
+
+* file structure 
+```
+{
+    "apiKey": "API_KEY_WATSON",
+    "iam_api_key_description": "IAM_API_KEY_DESCRIPTION",
+    "iam_api_key_name": "IAM_API_KEY_NAME",
+    "iam_role_crn": "IAM_ROLE_CRN",
+    "iam_serviceid_crn": "IAM_SERVICEID_CRN",
+    "url": "URL"
+}
+```
--- robots/text.js
+++ robots/text.js
@@ -1,12 +1,24 @@
 const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sbd = require('sbd')
+const watsonApiKey = require('../credentials/watson-nlu.json').apiKey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
 
+
+var nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'http://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
 
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
+
+
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
@@ -53,6 +65,36 @@ async function robot(content) {
         })
     }
 
+}
+
+function limitMaximumSentences(content){
+    content.sentences = content.sentences.slice(0, content.maximumSentences)
+}
+
+async function fetchKeywordsOfAllSentences (content){
+    for (const sentence of content.sentences){
+        sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+    }
+}
+
+async function fetchWatsonAndReturnKeywords(sentence){
+    return new Promise((resolve, reject) =>{
+        nlu.analyze({
+            text: sentence,
+            features:{
+                keywords: {}
+            }
+        }, (error, response) => {
+            if (error){
+                throw error
+            }
+            const keywords = response.keywords.map((keyword) => {
+                return keyword.text
+            })
+
+            resolve(keywords)
+        })
+    })
 }
 
 module.exports = robot
